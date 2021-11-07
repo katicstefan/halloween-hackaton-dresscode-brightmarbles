84046a0bd7e32ecb55740a1ede805728d9c44d83
Stefan Katić <53113791+katicstefan@users.noreply.github.com>
2021-11-07 15:41:56 +0100

    Card Game completed, todo Card counter

--- src/App.js
+++ src/App.js
@@ -8,38 +8,54 @@ function App() {
   const [dimension, setDimension] = React.useState(null); // N - dimenzija tabele NxN
   const [primes, setPrimes] = React.useState([]); // K - Cinioci broja N
   const [pairSize, setPairSize] = React.useState(null); // Broj istih kartica (jedan od cinioca K)
-  const [values, setValues] = React.useState([]); // Vrednosti kartica
-  const [table, setTable] = React.useState([[]]); // Tablica sa vrednostima (matrica NxN dimenzije)
+
+  const [cards, setCards] = React.useState([]); // Stanje svih kartica (id, value, selected, solved)
+  const [selectedCards, setSelectedCards] = React.useState([]); // Id-evi selektovanih kartica
+  const [solvedCardsCounter, setSolvedCardsCounter] = React.useState(0); // Brojac resenih kartica
 
   const [stopwatch, setStopwatch] = React.useState(null); // Vrednost za vreme igranja
+  const [cardSelects, setCardSelects] = React.useState(0); // Vrednost za broj koliko puta su karte bile otkrivene
   const [isPlaying, setIsPlaying] = React.useState(false); // Boolean koji oznacava pocetak igre
   const [isStopped, setIsStopped] = React.useState(false); // Boolean koji oznacava da je igra zavrsena
+
+  const [isBlocked, setIsBlocked] = React.useState(false); // Boolean koji ukazuje da li je mogucnost klika na karticu blokiran (cekanje na animaciju)
 
   // Hook koji se poziva kada se 'dimension' promeni
   // Ovde se traze cinioci broja N
   React.useEffect(() => {
     setPrimes(primeFactor(dimension));
-    setIsPlaying(false);
-    setIsStopped(false);
   }, [dimension]);
 
   // Hook koji se poziva kada se 'primes' promeni
   // Odredjuje se broj istih kartica dalje
   React.useEffect(() => {
     setPairSize(null);
+  }, [primes]);
+
+  function startGame() {
+    setStopwatch(null);
+    setIsPlaying(true);
+    setIsStopped(false);
+    setSolvedCardsCounter(0);
+  }
+
+  function stopGame() {
+    setIsPlaying(false);
+    setIsStopped(true);
+  }
+
+  function resetGame() {
     setIsPlaying(false);
     setIsStopped(false);
-  }, [primes]);
+    if (pairSize != null) generateValues(pairSize);
+  }
 
   // Hook koji se poziva kada se 'pairSize' promeni
   React.useEffect(() => {
-    setIsPlaying(false);
-    setIsStopped(false);
+    resetGame();
   }, [pairSize]);
 
-  // Hook koji se poziva kada se 'values' promeni
-  React.useEffect(() => {}, [values]);
-
+  // Hook koji se poziva kada se 'isPlaying, isStopped' promene
   React.useEffect(() => {
     let interval = null;
 
@@ -53,9 +69,57 @@ function App() {
     };
   }, [isPlaying, isStopped]);
 
+  // Hook koji se poziva kada se 'selectedCards' promeni
+  React.useEffect(() => {
+    var matching = true;
+    for (var i = 0; i < selectedCards.length - 1; i++) {
+      matching =
+        selectedCards[i].value === selectedCards[i + 1].value
+          ? matching && true
+          : matching && false;
+    }
+    if (matching) {
+      console.log("matching");
+      if (selectedCards.length === pairSize) {
+        var tempArray = Object.values({ ...cards });
+        for (var i = 0; i < selectedCards.length; i++) {
+          tempArray[selectedCards[i].id] = updateCard(selectedCards[i]);
+        }
+        setCards(Object.values({ ...tempArray }));
+        setSolvedCardsCounter(solvedCardsCounter + pairSize);
+        setSelectedCards([]);
+      }
+    } else {
+      if (selectedCards.length === pairSize) {
+        var tempArray = Object.values({ ...cards });
+        for (var i = 0; i < selectedCards.length; i++) {
+          tempArray[selectedCards[i].id] = {
+            ...selectedCards[i],
+            selected: false
+          };
+        }
+        //timeout
+        setIsBlocked(true);
+        setTimeout(() => {
+          setCards(Object.values({ ...tempArray }));
+          setSelectedCards([]);
+          setIsBlocked(false);
+        }, 750);
+      }
+    }
+  }, [selectedCards]);
+
+  // Hook koji se poziva kada se 'cards' promeni
+  React.useEffect(() => {}, [cards]);
+
+  // Hook koji se poziva kada se 'solvedCardsCounter, cards' promeni (resavanje kartica)
+  React.useEffect(() => {
+    if (solvedCardsCounter === cards.length && cards.length > 0) stopGame();
+  }, [solvedCardsCounter, cards]);
+
   // Event handler - poziva se na promenu input polja za 'dimension' stanje
   function handleDimensionChange(e) {
-    console.log(`Dimension: ${e.target.value}`);
+    //console.log(`Dimension: ${e.target.value}`);
     var val = e.target.value;
     validateDimension(val) ? setDimension(val) : setDimension(null);
   }
@@ -73,17 +137,16 @@ function App() {
   // Event Handler - reaguje na klik na dugme za odabir velicine parova
   function handlePairsSelect(e, prime) {
     if (pairSize !== prime) {
-      console.log(`Pair size: ${prime}`);
+      //console.log(`Pair size: ${prime}`);
       setPairSize(prime);
-      console.log(`Total pairs: ${(dimension * dimension) / prime}`);
+      //console.log(`Total pairs: ${(dimension * dimension) / prime}`);
       generateValues(prime);
     }
   }
 
-  // Generise vrednosti za "N^2 / K" parova
+  // Generise vrednosti za "N^2 / K (prime)" parova
   function generateValues(prime) {
     var totalPairs = (dimension * dimension) / prime;
-    console.log(`Generate values for ${totalPairs} pairs.`);
     var _values = [];
     for (var i = 0; i < totalPairs; i++) {
       for (var j = 0; j < prime; j++) {
@@ -91,33 +154,62 @@ function App() {
       }
     }
     _values = shuffle(_values);
-    setValues(_values);
-    console.log(`Total values generated: ${_values.length}`);
-    fillTable(_values);
+    fillCards(_values);
   }
 
-  // Puni kvadratnu tablicu vrednostima
-  function fillTable(_values) {
-    var _table = [];
-    for (var i = 0; i < dimension; i++) {
-      _table[i] = [];
-      for (var j = 0; j < dimension; j++) {
-        _table[i][j] = _values[i * dimension + j];
-      }
+  function fillCards(_values) {
+    var _cards = [];
+    for (var i = 0; i < dimension * dimension; i++) {
+      _cards[i] = {
+        id: i,
+        value: _values[i],
+        selected: false,
+        solved: false
+      };
     }
-    setTable(_table);
+    setCards(_cards);
+    setSelectedCards([]);
+    setSolvedCardsCounter([]);
   }
 
   function handleButtonStart() {
-    setStopwatch(null);
-    setIsPlaying(true);
-    setIsStopped(false);
+    resetGame();
+    startGame();
   }
+
   function handleButtonStop() {
-    setIsPlaying(false);
-    setIsStopped(true);
+    stopGame();
     generateValues(pairSize);
   }
+
+  // Reveal card
+  function handleCardClick(e, card) {
+    // Ako kartica nije prethodno selektovana i ako nije resena
+    if (
+      !selectedCards.find((_card) => _card.id === card.id) &&
+      !card.solved &&
+      !isBlocked
+    )
+      updateSelectedCards(card);
+  }
+
+  function updateSelectedCards(card) {
+    card = { ...card, selected: true };
+    setSelectedCards(
+      Object.values({ ...selectedCards, [selectedCards.length]: card })
+    );
+    setCards(Object.values({ ...cards, [card.id]: { ...card } }));
+  }
+
+  function updateCard(card) {
+    card = {
+      ...card,
+      selected: false,
+      solved: true
+    };
+    return card;
+  }
+
   return (
     <div className="App">
       <div className="containerDimension">
@@ -176,17 +268,22 @@ function App() {
               Stop
             </button>
           ))}
-        {(isPlaying || isStopped) && `${stopwatch / 1000} s`}
+        <p>
+          {isStopped && "Game Over! Your time is "}
+          {(isPlaying || isStopped) && `${stopwatch / 1000} s`}
+        </p>
       </div>
-      {isPlaying && table[0].length > 0 && (
-        <div className="containerTable">
-          {table.map((row, i) => (
-            <span key={i} className="tableRow">
-              {row.map((col, j) => (
-                <span key={j} className="tableField">
-                  {col}
-                </span>
-              ))}
+      {isPlaying && cards.length > 0 && (
+        <div className="containerCards">
+          {cards.map((card) => (
+            <span
+              key={card.id}
+              className={`card ${card.selected ? " selected" : ""}${
+                card.solved ? " solved" : ""
+              }`}
+              onClick={() => handleCardClick(this, card)}
+            >
+              {card.value}
             </span>
           ))}
         </div>
